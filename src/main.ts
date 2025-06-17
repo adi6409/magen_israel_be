9cdfc92b0e507f05c994b3409fd3b6c5eacbe1a0
Adi Stroianu <astroianu530@gmail.com>
2025-06-17 20:21:19 +0300

    Add custom WebSocket adapter to set API path for socket connections

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { IoAdapter } from '@nestjs/platform-socket.io';
+
+class CustomIoAdapter extends IoAdapter {
+  createIOServer(port: number, options?: any) {
+    options = options || {};
+    options.path = '/api/ws';
+    return super.createIOServer(port, options);
+  }
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useWebSocketAdapter(new CustomIoAdapter(app));
   // add /api prefix
   app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3939);
